ba2a8dce4f652d764f81bab39c59fd3d9f8d46e2
Allegra9 <aukseallegra@gmail.com>
2021-09-12 10:01:26 +0100

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import PostCreate from "./components/PostCreate";
+import PostList from "./components/PostList";
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:4000/posts");
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate fetchPosts={fetchPosts} />
+      <hr />
+      <h1>Posts</h1>
+      <PostList posts={Object.values(posts)} />
     </div>
   );
-}
-
+};
 export default App;
