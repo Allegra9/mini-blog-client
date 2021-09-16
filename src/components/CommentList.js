cd9a5693d043f20b83b108201e613926339c91c5
Allegra9 <aukseallegra@gmail.com>
2021-09-16 15:32:12 +0100

    Render comment based on it's moderation state

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+const contentWithState = comment => {
+  const { state, content } = comment
+  if (state === 'pending') return 'pending moderation'
+  if (state === 'rejected') return 'comment suspended'
+  return content
+}
+
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>
+    return <li key={comment.id}>{contentWithState(comment)}</li>
   })
 
   return <ul>{renderedComments}</ul>
